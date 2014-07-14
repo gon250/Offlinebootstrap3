d24af7a51fe627f4bba36e1b7ea9404e67c73087
Gonzalo <zalo250@gmail.com>
2014-07-14 16:35:23 +0100

    width problems solved.

--- background.js
+++ background.js
@@ -1,2 +1,2 @@
 //To open it in a different in a new windows
-window.open(chrome.extension.getURL("index.html"),"gc-popout-window","width=960px,height=96000px")
+//window.open(chrome.extension.getURL("index.html"),"gc-popout-window","width=960px,height=96000px")
